44acce45bdb988723d86c5b4006fc469fb64b1a2
Bruno Gama <bgamap@gmail.com>
2018-03-21 14:36:17 -0300

    Initial commit

---
+++ GistCommenterTests/GistCommenterTests.swift
@@ -0,0 +1,13 @@
+//
+//  GistCommenterTests.swift
+//  GistCommenterTests
+//
+//  Created by Bruno Gama on 20/03/2018.
+//  Copyright © 2018 Bruno Gama. All rights reserved.
+//
+
+@testable import GistCommenter
+import XCTest
+
+internal class GistCommenterTests: XCTestCase {
+}
---
+++ GistCommenterUITests/GistCommenterUITests.swift
@@ -0,0 +1,12 @@
+//
+//  GistCommenterUITests.swift
+//  GistCommenterUITests
+//
+//  Created by Bruno Gama on 20/03/2018.
+//  Copyright © 2018 Bruno Gama. All rights reserved.
+//
+
+import XCTest
+
+internal class GistCommenterUITests: XCTestCase {
+}
