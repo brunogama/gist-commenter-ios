b75924660e00442e7f42bd103165fbc25a054eb3
Bruno Gama <bgamap@gmail.com>
2018-03-23 17:19:17 -0300

    Added GistComment model, and added .comments endpoint to the GistService

---
+++ GistCommenter/Entities/GistComment.swift
@@ -0,0 +1,22 @@
+//
+//  GistComment.swift
+//  GistCommenter
+//
+//  Created by Bruno Gama on 23/03/2018.
+//  Copyright © 2018 Bruno Gama. All rights reserved.
+//
+
+import Foundation
+
+internal struct GistComment: Codable, CodableExtension {
+    let id: Int
+    let url, body: String
+    let user: User
+    let createdAt, updatedAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, url, body, user
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+}
